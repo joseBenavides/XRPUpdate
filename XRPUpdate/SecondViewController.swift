18bcdc5976b13750334951d378c9b353b160736f
Jose Benavides <gammahacks@yahoo.com>
2018-06-09 10:37:53 -0700

    Initial Commit

---
+++ XRPUpdate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  XRPUpdate
+//
+//  Created by Jose on 6/9/18.
+//  Copyright © 2018 Jose Benavides. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
